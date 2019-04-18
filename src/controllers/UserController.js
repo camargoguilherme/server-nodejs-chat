9835054f1972c2801e9fea685733bc2394146440
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2019-04-18 17:29:31 -0300

    Configurando autenticacao por jwt

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,85 +1,127 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
-class UserController{
-  async store(req, res){
+class UserController {
+  async store(req, res) {
     // create a user
     console.log(req.body)
 
-    await User.create(req.body, function(error, user){
-      if(error){
+    await User.create(req.body, function (error, user) {
+      if (error) {
         res.json(error);
       }
-      userJoin(req, user.toJson());
+      user.token = user.toJson().token
+      //user.save()
+      userJoin(req, user.toJson())
       res.json(user.toJson())
     });
-    
-    
   }
 
-  async update(req, res){
+  async update(req, res) {
     // create a user
     console.log(req.body)
 
-    await User.findOneAndUpdate(
-      { email: req.body.email }, 
-      req.body,
-      {new: true},
-      function(error, user){
-        if(error){
+    await User.findOneAndUpdate({
+        email: req.body.email
+      },
+      req.body, {
+        new: true
+      },
+      function (error, user) {
+        if (error) {
           res.json(error)
         }
         user.save();
         res.json(user.toJson());
       });
-    
+
   }
 
-  async authenticate(req, res){
-    const { username } = req.body
+  async authenticate(req, res) {
+    const {
+      username
+    } = req.body
 
     console.log(req.body)
 
-    await User.findOneAndUpdate( { username }, { logged: true }, function (err, user) {
-      
+    await User.findOneAndUpdate({
+      username
+    }, {
+      logged: true
+    }, function (err, user) {
+
       if (err) {
         return err
       } else if (!user) {
-        return res.json({ auth: false, message: 'Erro ao realizar login, usuário não encontrado'});
+        return res.json({
+          auth: false,
+          message: 'Erro ao realizar login, usuário não encontrado'
+        });
       }
-      if(!user.authenticateUser(req.body.password)){
-        return res.json({ auth: false, message: 'Usuário ou senha inválidos'});
+      if (!user.authenticateUser(req.body.password)) {
+        return res.json({
+          auth: false,
+          message: 'Usuário ou senha inválidos'
+        });
       }
       userJoin(req, user.toJson())
       return res.json(user.toJson());
     });
   }
-  
+
   // verify 
-  async isAuthenticate (req, res){
+  async isAuthenticate(req, res, next) {
     const token = req.headers['x-access-token'];
-    const user = await User.findById(req.headers.id);
-    console.log(`isAuthenticate: ${JSON.stringify(user)}`)
-    if (!token) 
-      return res.json({ auth: false, message: 'Nenhum token fornecido' });
-    
-    jwt.verify(token, process.env.JWT_WORD || 'JWT_WORD', function(err, decoded) {
-      if (err) 
-        return res.json({ auth: false, message: 'Falha ao autenticar token' });
-      
-      userJoin(req, user.toJson());
-      return res.json({ auth: true });
+
+    if (!token)
+      return res.json({
+        auth: false,
+        message: 'Nenhum token fornecido'
+      });
+
+    jwt.verify(token, process.env.JWT_WORD || 'JWT_WORD', function (err, decoded) {
+      if (err)
+        return res.json({
+          auth: false,
+          message: 'Falha ao autenticar token'
+        });
+
+      return next();
     });
-  }  
+  }
+
+  async findAll(req, res) {
+    const users = await User.find({
+      logged: true
+    },{
+      _id: 1,
+      username: 1,
+      name: 1,
+      email: 1
+    })
+    
+    console.log(users)
+
+    res.json(users)
+  }
 }
 
 async function userJoin(req, user) {
-  await User.findOneAndUpdate( { id: user._id }, { logged: true, ...user }, {new: true} );
+  await User.findOneAndUpdate({
+    id: user._id
+  }, {
+    logged: true,
+    ...user
+  }, {
+    new: true
+  });
 
-  const users = await User.find({logged: true})
+  const users = await User.find({
+    logged: true
+  })
   console.log(`${users.length} users logged`)
   req.io.sockets.emit('join', users);
-  
+
 }
 
 module.exports = new UserController();
